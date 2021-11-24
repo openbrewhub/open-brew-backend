719d32838a793d356c45f6dac17a9e76aea84ba7
René Fehlow <rene.fehlow@web.de>
2021-11-24 21:04:06 +0100

    Added test lambda

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -10,11 +10,18 @@ export class InfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // defines an AWS Lambda resource
+    const hello = new lambda.Function(this, 'HelloHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'                // file is "hello", function is "handler"
+    });
+
+    /*
     const api = new apigateway.SpecRestApi(this, 'recipes-api', {
       apiDefinition: apigateway.ApiDefinition.fromAsset('./assets/openapi-definition.yml')
     });
 
-    /*
     Ist nicht so geil wie angenommen... müssen das erstmal anlegen und dann exportieren, weil da im swagger aws-spezifische Params stehen müssen.
 
     z. B.:
